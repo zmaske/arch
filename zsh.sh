9d475dec7d6e8236a905db004a486beb29c43a52
zmaske <53617185+zmaske@users.noreply.github.com>
2021-08-27 05:09:07 +0000

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,4 @@
 su $1
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-echo -e "yes\nsed -i 's/robbyrussell/agnoster/g' ~/.zshrc\nexit" | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e "yes\nsed -i 's/robbyrussell/agnoster/g' ~/.zshrc" | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
